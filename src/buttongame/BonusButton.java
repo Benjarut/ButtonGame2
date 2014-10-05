0b16b963854bcec79e36b808ecd6f0826e983251
CinDy <CinDy@cindy-vaio.com>
2014-10-05 19:46:26 +0700

    have bonusbutton's image

---
+++ src/buttongame/BonusButton.java
@@ -0,0 +1,23 @@
+package buttongame;
+
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
+
+public class BonusButton extends buttonPress{
+	private static final int width=76;
+	private static final int height=53;
+	public BonusButton() throws SlickException {
+		super();
+	}
+	public void addImage() throws SlickException {
+		image=new Image("res/bonusButton.png");
+	} 
+	
+	public static int getWidth(){
+		return width;
+	}
+	public static int getHeight(){
+		return height;
+	}
+
+}
